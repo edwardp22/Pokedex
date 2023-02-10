0154e7bea2ece54ab3c7bd1bb0fdde579a29fcfe
Edward Peña <58954908+edwardp22@users.noreply.github.com>
2023-02-10 11:30:15 -0800

    Create shared folder
    It is continaing the base url of the sprites.

---
+++ src/shared/index.ts
@@ -0,0 +1,6 @@
+/**
+ * The GraphQL endpoint does not return a valid URL.
+ * In their GitHub issues page people has posted a workaround by using a default base URL and adding the rest.
+ */
+export const spritesBaseURL =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/";
