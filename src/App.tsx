f592d095c7bdd6aa614b0fa21a5a11cb7abb9909
Edward Peña <58954908+edwardp22@users.noreply.github.com>
2023-02-10 11:34:14 -0800

    Update App.tsx
    Initial screen with pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+import { CssBaseline, Grid, Typography } from "@mui/material";
+
+import usePokemon from "./api/pokemon";
+import { Card } from "./common";
+
 function App() {
-  return <div></div>;
+  const { data, loading } = usePokemon();
+
+  if (loading) return <Typography variant="body1">Loading...</Typography>;
+
+  return (
+    <>
+      <CssBaseline />
+
+      <Grid container spacing={1}>
+        {data.map((pokemon) => (
+          <Card
+            key={"pokemon" + pokemon.id}
+            pokemon={pokemon}
+            onClick={(pokemon) => console.log(pokemon)}
+          />
+        ))}
+      </Grid>
+    </>
+  );
 }
 
 export default App;
