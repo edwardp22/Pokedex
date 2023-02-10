594ab71efef7db2a67950ac9ac87dc023014add1
Edward Peña <58954908+edwardp22@users.noreply.github.com>
2023-02-10 11:33:10 -0800

    Modify Star component
    adding functionality onClick.
    destructure values in styles for more clarity.

--- src/common/star/Star.tsx
+++ src/common/star/Star.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+
 import { Star as StarMUI } from "@mui/icons-material";
 
 import styles from "./star.module.css";
 
-export default function Star({ selected }: { selected?: boolean }) {
-  return <StarMUI className={selected ? styles.selected : styles.unselected} />;
+export default function Star({
+  selected,
+  onClick,
+}: {
+  selected?: boolean;
+  onClick: () => void;
+}) {
+  const { starSelected, starUnselected } = styles;
+
+  return (
+    <i onClick={() => onClick()}>
+      <StarMUI className={selected ? starSelected : starUnselected} />
+    </i>
+  );
 }
